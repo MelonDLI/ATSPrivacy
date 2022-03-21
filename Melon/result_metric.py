7cffe930306571fa3c721744c44b7c43e7957795
MelonDLI <melon_forever@163.com>
2022-03-21 22:55:49 +1100

    update reconstruction result

--- Melon/result_metric.py
+++ Melon/result_metric.py
@@ -1,13 +1,13 @@
 import numpy as np
-file = '/home/remote/u7076589/ATSPrivacy/Melon/benchmark/images_ResNet20_50/metric.npy'
+file = '/home/remote/u7076589/ATSPrivacy/Melon/benchmark/images/metric.npy'
 # file = '/home/remote/u7076589/ATSPrivacy/Melon/benchmark/images/metric.npy'
 result = np.load(file,allow_pickle=True)
 print('number of result:{}'.format(len(result)))
 sum_ = 0
 max_psnr = 0
-for i in range(43):
+for i in range(len(result)):
     sum_+=result[i]['test_psnr']
     if result[i]['test_psnr']>max_psnr:
         max_psnr = result[i]['test_psnr']
-print(sum_/43) #10.424027647972107
+print(sum_/len(result)) #10.424027647972107
 print(max_psnr)
