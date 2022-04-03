b12cace874ad4d7761c77e2c7c0688fbb39be588
MelonDLI <melon_forever@163.com>
2022-04-04 08:15:12 +1000

    ResNet MoEx +DA done

--- result_metric_MoEx.py
+++ result_metric_MoEx.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-#! ResNet 3-1-7+43-18-18
-file = '/home/remote/u7076589/ATSPrivacy/benchmark/images/data_cifar100_arch_ResNet20-4_epoch_200_optim_inversed_mode_aug_auglist_3-1-7+43-18-18_rlabel_False/metric.npy'
+#! MoEx 3-1-7+43-18-18
+file = '/home/remote/u7076589/ATSPrivacy/benchmark/images/MoEx_data_cifar100_arch_ResNet20-4_epoch_200_optim_inversed_mode_aug_auglist_3-1-7+43-18-18_rlabel_False/metric.npy'
 result = np.load(file,allow_pickle=True)
 sum_ = 0
 max_psnr = 0
@@ -15,8 +15,8 @@ print('ResNet+3-1-7+43-18-18:')
 print('average: {}'.format(sum_/len(result)))
 print('max:{}'.format(max_psnr))
 
-#! ResNet 43-18-18
-file = '/home/remote/u7076589/ATSPrivacy/benchmark/images/data_cifar100_arch_ResNet20-4_epoch_200_optim_inversed_mode_aug_auglist_43-18-18_rlabel_False/metric.npy'
+#! MoEx 43-18-18
+file = '/home/remote/u7076589/ATSPrivacy/benchmark/images/MoEx_data_cifar100_arch_ResNet20-4_epoch_200_optim_inversed_mode_aug_auglist_43-18-18_rlabel_False/metric.npy'
 result = np.load(file,allow_pickle=True)
 sum_ = 0
 max_psnr = 0
@@ -30,8 +30,8 @@ print('ResNet+43-18-18:')
 print('average: {}'.format(sum_/len(result)))
 print('max:{}'.format(max_psnr))
 
-#! ResNet 3-1-7
-file = '/home/remote/u7076589/ATSPrivacy/benchmark/images/data_cifar100_arch_ResNet20-4_epoch_200_optim_inversed_mode_aug_auglist_3-1-7_rlabel_False/metric.npy'
+#! MoEx 3-1-7
+file = '/home/remote/u7076589/ATSPrivacy/benchmark/images/MoEx_data_cifar100_arch_ResNet20-4_epoch_200_optim_inversed_mode_aug_auglist_3-1-7_rlabel_False/metric.npy'
 result = np.load(file,allow_pickle=True)
 sum_ = 0
 max_psnr = 0
@@ -45,8 +45,8 @@ print('ResNet+3-1-7:')
 print('average: {}'.format(sum_/len(result)))
 print('max:{}'.format(max_psnr))
 
-#! ResNet
-file = '/home/remote/u7076589/ATSPrivacy/benchmark/images/data_cifar100_arch_ResNet20-4_epoch_200_optim_inversed_mode_aug_auglist__rlabel_False/metric.npy'
+#! MoEx
+file = '/home/remote/u7076589/ATSPrivacy/benchmark/images/MoEx_data_cifar100_arch_ResNet20-4_epoch_200_optim_inversed_mode_aug_auglist__rlabel_False/metric.npy'
 result = np.load(file,allow_pickle=True)
 sum_ = 0
 max_psnr = 0
